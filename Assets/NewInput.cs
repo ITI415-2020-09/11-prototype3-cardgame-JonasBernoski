4faaec2f2438546e0b227ffde0768beb2abf49c3
Jonas Bernoski <jonas2015@aol.com>
2020-12-01 16:49:45 -0500

    Clicking issues

--- Assets/NewInput.cs
+++ Assets/NewInput.cs
@@ -7,7 +7,7 @@ public class NewInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetMouseClick();
     }
 
     // Update is called once per frame
@@ -15,4 +15,49 @@ public class NewInput : MonoBehaviour
     {
         
     }
+    void GetMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("Deck"))
+                {
+                    Deck();
+                }
+                else if (hit.collider.CompareTag("Card"))
+                {
+                    Card();
+                }
+                else if (hit.collider.CompareTag("Top"))
+                {
+                    Top();
+                }
+                else if (hit.collider.CompareTag("Bottom"))
+                {
+                    Bottom();
+                }
+            }
+        }
+    }
+    void Deck()
+    {
+        print("clicked");
+    }
+    void Card()
+    {
+        print("clicked");
+    }
+    void Top()
+    {
+        print("clicked");
+    }
+    void Bottom()
+    {
+        print("clicked");
+    }
 }
+
